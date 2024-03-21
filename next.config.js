cf24171768a7ffb7b780f4bdac9405995cfd5fe0
Jamie Speller <j@miee.uk>
2024-03-21 01:10:33 +0000

    fix nextjs

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	basePath: "/tfl-journey-planner",
+	assetPrefix: "/tfl-journey-planner/",
+	// output: "export",
+	reactStrictMode: true,
+};
+
+module.exports = nextConfig;
